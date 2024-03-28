14839bc6f58a18e02fce319599bc8bf6cc421d5c
JuanAste <juanpabloaste00@gmail.com>
2024-03-28 19:24:55 -0300

    clien whatsapp web

--- src/app/api/procesarExcel/route.js
+++ src/app/api/procesarExcel/route.js
@@ -2,7 +2,11 @@ import twilio from "twilio";
 import * as xlsx from "xlsx";
 const fs = require("fs");
 
+import { Client } from 'whatsapp-web.js';
+import QRCode from 'qrcode-terminal';
+
 import { NextResponse } from "next/server";
+import { numberToHour } from "@/utils/numberToHour";
 
 export async function POST(req) {
   try {
@@ -19,20 +23,31 @@ export async function POST(req) {
 
     console.log(data, "data");
 
-    const accountSid = process.env.ACCOUNT_SID;
-    const apiKey = process.env.API_KEY;
-    const apiSecret = process.env.API_SECRET;
-    const fromWhatsAppNumber = process.env.NUMBER;
-    const client = twilio(apiKey, apiSecret, { accountSid: accountSid });
+      // Inicializa el cliente de WhatsApp Web JS
+      const client = new Client();
+      client.on('qr', qr => {
+          QRCode.generate(qr, {small: true});
+      });
+      
+      client.on('ready', async () => {
+          console.log('Client is ready!');
+          
+          // Dentro de la función handler, después de extraer los datos
+          for (const { Hora, Nombre, Numero } of data) {
+            const hour = numberToHour(Hora);
+            try {
+              // Asegúrate de que el número tenga el formato correcto, incluyendo el código de país
+              await client.sendMessage(`${Numero}@c.us`, `Hola ${Nombre}, este es un mensaje de prueba, la hora es ${hour}.`);
+            } catch (error) {
+              console.error(error); // Captura y loguea cualquier error
+            }
+          }
+  
+          client.destroy(); // Cierra la sesión del cliente después de enviar los mensajes
+      });
+  
+      await client.initialize();
 
-    // Dentro de la función handler, después de extraer los datos
-    // data.forEach(async ({ Nombre, Telefono }) => {
-    //   await client.messages.create({
-    //     body: `Hola ${Nombre}, este es un mensaje de prueba.`,
-    //     from: fromWhatsAppNumber,
-    //     to: `whatsapp:${Telefono}`,
-    //   });
-    // });
     return NextResponse.json(
       { message: "Procesado correctamente" },
       { status: 200 }
